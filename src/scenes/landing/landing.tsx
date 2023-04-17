b42621d816acba85b1ccc500e451b97c9268197b
Addison Dalton <zarch14@gmail.com>
2023-04-16 23:15:22 -0400

    added feature projects section to landing

--- src/scenes/landing/landing.tsx
+++ src/scenes/landing/landing.tsx
@@ -1,12 +1,13 @@
-import { Text } from '@chakra-ui/react';
-import { MdPerson, MdPalette } from 'react-icons/md';
-import { GiPalette } from 'react-icons/gi';
+import { Text, Flex } from '@chakra-ui/react';
+import { MdPerson } from 'react-icons/md';
+
 import Section from 'components/section';
+import LandingProjects from './sections/projects';
 const Landing = () => {
   const introText =
-    "I'm a software engineer with a love for web development and a passion for all things code. When I'm not crafting solutions to complex problems, you can find me indulging in a few of my favorite pastimes: baking up a storm, hitting the pavement for a run, or losing myself in a video game. Oh, and did I mention I have two adorable corgis who keep me on my toes? So, let's dive into what I've been working on lately!";
+    "I'm a software engineer with a love for web development and a passion for all things code. When I'm not crafting solutions to complex problems, you can find me enjoying in a few of my favorite pastimes: baking up a storm, hitting the pavement for a run, or losing myself in a video game. Oh, and did I mention I have two adorable corgis who keep me on my toes? So, let's dive into what I've been working on lately!";
   return (
-    <>
+    <Flex direction="column" alignItems={{ base: 'center', md: 'start' }}>
       {/* Intro */}
       <Section
         title="Hi, I'm Addison"
@@ -19,19 +20,8 @@ const Landing = () => {
         </Text>
       </Section>
       {/* Projects */}
-      <Section
-        title="My creations!"
-        variant="link"
-        href="/projects"
-        icon={MdPalette}
-      >
-        <Text fontSize="lg" maxWidth="600px">
-          {
-            "I'd like to show a tile of my latest project here. This probably means I should create a JSON or something to hold project data, and I'll show the most recent. Below that just some generic info about what I like to work on"
-          }
-        </Text>
-      </Section>
-    </>
+      <LandingProjects />
+    </Flex>
   );
 };
 export default Landing;
