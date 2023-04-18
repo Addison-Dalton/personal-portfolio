7c6df20bb8e57ee0875e1dc04dfa5bbe3f01ed48
Addison Dalton <zarch14@gmail.com>
2023-04-17 23:06:54 -0400

    created project image component

---
+++ src/components/project-image/project-image.tsx
@@ -0,0 +1,109 @@
+import { Flex, Box, Image } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
+import { FaGithub } from 'react-icons/fa';
+import styled from '@emotion/styled';
+
+import { useColor } from 'utils/hooks';
+import IconLink from 'components/icon-link';
+
+type Props = {
+  imageSrc: string;
+  imageAlt?: string;
+  githubUrl?: string;
+  projectUrl?: string;
+};
+
+const ImageContainer = styled(Box)`
+  position: relative;
+  overflow: hidden;
+  transition: border 0.2s ease-in;
+
+  &:hover .image-overlay {
+    display: block;
+    opacity: 1;
+  }
+`;
+
+const ProjectImage = ({ imageSrc, imageAlt, githubUrl, projectUrl }: Props) => {
+  const {
+    colors: [primaryBgColor]
+  } = useColor(['primaryBg']);
+
+  return (
+    <Box rounded="md" boxShadow="dark-lg">
+      <ImageContainer
+        roundedTop="md"
+        roundedBottom={{ base: '', lg: 'md' }}
+        boxSize={['xs', 'sm']}
+      >
+        <Box
+          position="absolute"
+          top={0}
+          left={0}
+          width="100%"
+          height="100%"
+          className="image-overlay"
+          backgroundColor="rgba(44, 194, 159, 0.5)"
+          opacity={0}
+          transition="opacity 0.2s linear"
+        >
+          <Flex
+            justifyContent="space-between"
+            alignItems="center"
+            maxWidth="40%"
+            height="100%"
+            margin="auto"
+          >
+            {githubUrl && (
+              <IconLink
+                icon={FaGithub}
+                href={githubUrl}
+                fontSize="6xl"
+                isExternal
+              />
+            )}
+            {projectUrl && (
+              <IconLink
+                icon={ExternalLinkIcon}
+                href={projectUrl}
+                fontSize="6xl"
+                isExternal
+              />
+            )}
+          </Flex>
+        </Box>
+        <Image src={imageSrc} alt={imageAlt} />
+      </ImageContainer>
+      <Box
+        display={{ base: 'flex', lg: 'none' }}
+        justifyContent="center"
+        gap={10}
+        roundedBottom="md"
+        width="100%"
+        padding={1}
+        paddingTop={2}
+        backgroundColor={primaryBgColor}
+      >
+        {/* TODO - link color against background not great in light mode */}
+        {githubUrl && (
+          <IconLink
+            icon={FaGithub}
+            href={githubUrl}
+            fontSize="3xl"
+            isExternal
+          />
+        )}
+        {projectUrl && (
+          <IconLink
+            icon={ExternalLinkIcon}
+            href={projectUrl}
+            fontSize="3xl"
+            isExternal
+          />
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default ProjectImage;
--- src/scenes/landing/sections/projects.tsx
+++ src/scenes/landing/sections/projects.tsx
@@ -1,29 +1,15 @@
-import { Text, Flex, Box, Image, Link, Icon } from '@chakra-ui/react';
-import { ExternalLinkIcon } from '@chakra-ui/icons';
-import styled from '@emotion/styled';
+import { Text, Flex, Link } from '@chakra-ui/react';
 import { Link as RRLink } from 'react-router-dom';
 import { MdPalette } from 'react-icons/md';
-import { FaGithub } from 'react-icons/fa';
 
 import { useColor } from 'utils/hooks';
 import Section from 'components/section';
-import IconLink from 'components/icon-link';
-
-const ImageContainer = styled(Box)`
-  position: relative;
-  overflow: hidden;
-  transition: border 0.2s ease-in;
-
-  &:hover .image-overlay {
-    display: block;
-    opacity: 1;
-  }
-`;
+import ProjectImage from 'components/project-image';
 
 const LandingProjects = () => {
   const {
-    colors: [primaryBgColor, secondaryColor]
-  } = useColor(['primaryBg', 'secondary']);
+    colors: [secondaryColor]
+  } = useColor(['secondary']);
 
   return (
     <Section
@@ -38,72 +24,12 @@ const LandingProjects = () => {
         gap={12}
         wrap="wrap"
       >
-        {/* TODO - extract image stuff to component */}
-        <Box rounded="md" boxShadow="dark-lg">
-          <ImageContainer
-            roundedTop="md"
-            roundedBottom={{ base: '', lg: 'md' }}
-            boxSize={['xs', 'sm']}
-          >
-            <Box
-              position="absolute"
-              top={0}
-              left={0}
-              width="100%"
-              height="100%"
-              className="image-overlay"
-              backgroundColor="rgba(44, 194, 159, 0.5)"
-              opacity={0}
-              transition="opacity 0.2s linear"
-            >
-              <Flex
-                justifyContent="space-between"
-                alignItems="center"
-                maxWidth="40%"
-                height="100%"
-                margin="auto"
-              >
-                <IconLink
-                  icon={FaGithub}
-                  href="https://github.com/Addison-Dalton/pokedexle"
-                  fontSize="6xl"
-                  isExternal
-                />
-                <IconLink
-                  icon={ExternalLinkIcon}
-                  href="https://pokedexle.app"
-                  fontSize="6xl"
-                  isExternal
-                />
-              </Flex>
-            </Box>
-            <Image src="/pokedexle.png" alt="Pokedexle" />
-          </ImageContainer>
-          <Box
-            display={{ base: 'flex', lg: 'none' }}
-            justifyContent="center"
-            gap={10}
-            roundedBottom="md"
-            width="100%"
-            padding={1}
-            paddingTop={2}
-            backgroundColor={primaryBgColor}
-          >
-            {/* TODO - link color against background not great in light mode */}
-            <IconLink
-              icon={FaGithub}
-              href="https://github.com/Addison-Dalton/pokedexle"
-              fontSize="3xl"
-              isExternal
-            />
-            <IconLink
-              icon={ExternalLinkIcon}
-              href="https://pokedexle.app"
-              fontSize="3xl"
-              isExternal
-            />
-          </Box>
-        </Box>
+        <ProjectImage
+          imageSrc="/pokedexle.png"
+          imageAlt="pokedexle"
+          githubUrl="https://github.com/Addison-Dalton/pokedexle"
+          projectUrl="https://pokedexle.app"
+        />
         <Text fontSize="lg" minWidth="300px" maxWidth="500px" flex={1}>
           {
             'Pokédexle is one of my favorite projects. I, like many, got caught up in the Wordle craze of early 2022. I decided to combine that with my love of Pokémon, and Pokédexle is the result! You can checkout my other work '
