0f76fa482ec1117c9cb742c318639b2b5d8dae2b
Addison Dalton <zarch14@gmail.com>
2023-05-13 21:45:53 -0400

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigpaths from 'vite-tsconfig-paths';
-import path from 'path';
-
-const generateAlias = (route: string) => path.resolve(__dirname, route);
 
 // https://vitejs.dev/config/
 export default defineConfig({
